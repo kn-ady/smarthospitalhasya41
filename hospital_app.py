692c800322b6def977028a7fd5962409c2a30887
kn-ady <ady@kodingnext.com>
2026-07-07 09:56:59 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import os
 
-st.set_page_config(page_title"Smart Hosipital Navigator",page_icon = "🏥",layout="wide")
+st.set_page_config(page_title="Smart Hosipital Navigator",page_icon = "🏥",layout="wide")
 
 st.title("Smart Hospital Navigator")
 
